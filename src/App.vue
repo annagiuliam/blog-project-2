32af23ad95431c8a6d80b82c2c7552226c0c520f
Anna <annagiulia.musso@gmail.com>
2022-01-03 14:58:25 +0100

    corrected imports in app

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,18 @@
     <PostDialog />
 
     <router-view />
-
-    <!-- <router-link to="/test-page">See Post</router-link> -->
   </v-app>
 </template>
 
 <script>
 import InputDialog from "./components/InputDialog.vue";
 import PostDialog from "./components/PostDialog.vue";
-// import PostTile from "./components/PostTile.vue";
-// import Filters from "./components/Filters.vue";
 
 export default {
   name: "App",
+  components: {
+    InputDialog,
+    PostDialog,
+  },
 };
 </script>
