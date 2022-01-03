595a8a14bb3ced6a7f4a5e42e1a5a2db93636d35
Anna <annagiulia.musso@gmail.com>
2022-01-03 17:30:33 +0100

    added PostPage

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 
 import Home from "./../views/Home";
+import PostPage from "./../views/PostPage";
 import TestPage from "./../views/TestPage";
 
 Vue.use(Router);
@@ -9,6 +10,7 @@ Vue.use(Router);
 export default new Router({
   routes: [
     { path: "/", component: Home },
+    { path: "/post-page/:id", name: "post-page", component: PostPage },
     { path: "/test-page", component: TestPage },
   ],
 });
---
+++ src/views/PostPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{ this.$route.params.id }}</div>
+    <PostContent :post="post" />
+  </div>
+</template>
+
+<script>
+import PostContent from "./../components/PostContent";
+export default {
+  name: "PostPage",
+  data() {
+    return {
+      postId: this.$route.params.id,
+    };
+  },
+  computed: {
+    post() {
+      return this.$store.state.posts.find((post) => post.id === this.postId);
+    },
+  },
+  components: { PostContent },
+};
+</script>
+
+<style></style>
