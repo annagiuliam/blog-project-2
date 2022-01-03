a24fd6c58714859a53f685996f23d1370a8bee28
Anna <annagiulia.musso@gmail.com>
2022-01-03 14:53:42 +0100

    created home route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
+
+import Home from "./../views/Home";
 import TestPage from "./../views/TestPage";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: [{ path: "/test-page", component: TestPage }],
+  routes: [
+    { path: "/", component: Home },
+    { path: "/test-page", component: TestPage },
+  ],
 });
