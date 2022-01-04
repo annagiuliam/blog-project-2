b87664563bfbbb5a185da3c248ab008e6ab387a3
Anna <annagiulia.musso@gmail.com>
2022-01-04 16:28:08 +0100

    added v-card layout to post in postPage

--- src/views/PostPage.vue
+++ src/views/PostPage.vue
@@ -1,7 +1,13 @@
 <template>
-  <div>
-    <PostContent :post="this.post" />
-  </div>
+  <v-container>
+    <v-row class="mt-8">
+      <v-col class="mx-auto" cols="11" md="10">
+        <v-card min-width="250px">
+          <PostContent :post="this.post" />
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
