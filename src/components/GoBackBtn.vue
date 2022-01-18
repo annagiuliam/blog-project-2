38b8e3f7abc12a272f72f5461e9a5e2df279bf60
Anna <annagiulia.musso@gmail.com>
2022-01-18 11:12:16 +0100

    added tooltip to go back btn

--- src/components/GoBackBtn.vue
+++ src/components/GoBackBtn.vue
@@ -1,24 +1,50 @@
 <template>
   <v-row>
-    <v-btn
-      outlined
-      fab
-      color="indigo"
-      class="ma-2"
-      @click="goBack"
-    >
-      <v-icon>mdi-arrow-left</v-icon>
-    </v-btn>
+    <div style="position: relative">
+      <Tooltip
+        v-if="showTooltip"
+        class="location"
+      >
+        {{ $gettext('go back') }}
+      </Tooltip>
+      <v-btn
+        outlined
+        fab
+        color="indigo"
+        class="ma-2"
+        @click="goBack"
+        @mouseover="showTooltip = true"
+        @mouseleave="showTooltip = false"
+      >
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+    </div>
   </v-row>
 </template>
 
 <script>
+import Tooltip from './Tooltip.vue'
 export default {
   name: 'GoBackBtn',
+  components: { Tooltip },
+  data () {
+    return {
+      showTooltip: false
+    }
+  },
   methods: {
     goBack () {
       return this.$router.back()
     }
   }
+
 }
 </script>
+<style scoped>
+.location {
+  top: 25%;
+  left: 110%;
+  width: 5rem;
+  transform: none
+}
+</style>
