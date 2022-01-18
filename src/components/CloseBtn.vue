3e18dc8ba3485cceb59c8583de06f476755ac94f
Anna <annagiulia.musso@gmail.com>
2022-01-18 11:11:51 +0100

    created closeBtn

---
+++ src/components/CloseBtn.vue
@@ -0,0 +1,45 @@
+<template>
+  <div style="position: relative">
+    <Tooltip
+      v-if="showTooltip"
+      class="location"
+    >
+      {{ $gettext('close') }}
+    </Tooltip>
+    <v-btn
+      class="ma-2"
+      outlined
+      fab
+      small
+      color="indigo"
+      @click="$emit('click')"
+      @mouseover="showTooltip = true"
+      @mouseleave="showTooltip = false"
+    >
+      <v-icon>mdi-close</v-icon>
+    </v-btn>
+  </div>
+</template>
+
+<script>
+import Tooltip from './Tooltip.vue'
+export default {
+  name: 'CloseBtn',
+  components: { Tooltip },
+  props: ['tooltipText'],
+  data () {
+    return {
+      showTooltip: false
+    }
+  }
+
+}
+</script>
+<style scoped>
+.location {
+  top: 25%;
+  left: -150%;
+  width: 5rem;
+  transform: none;
+}
+</style>
