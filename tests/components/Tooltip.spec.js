1b335e0882b574d52741bd019be167ea88600c3e
Anna <annagiulia.musso@gmail.com>
2022-01-27 15:41:18 +0100

    added tooltip tests

---
+++ tests/components/Tooltip.spec.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import Tooltip from '@/components/Tooltip.vue'
+
+Vue.use(Vuetify)
+
+function render () {
+  const div = document.createElement('div')
+  const wrap = document.createElement('div')
+  wrap.classList.add('v-application--wrap')
+  document.body.append(div, wrap)
+  return mount(Tooltip, {
+    vuetify: new Vuetify(),
+    attachTo: div,
+    mocks: { $gettext: utils.mocks.translate },
+    // added slots mouting option
+    slots: {
+      default: 'tooltip test'
+    }
+  })
+}
+
+describe('tooltip', () => {
+  it('renders', () => {
+    const wrapper = render()
+    expect(wrapper.isVisible()).toEqual(true)
+  })
+  it('show correct slot text', () => {
+    const wrapper = render()
+    expect(wrapper.html()).toContain('tooltip test')
+  })
+})
