3f884fd7c2738d0ab90606ca295a317af1eb8077
Anna <annagiulia.musso@gmail.com>
2022-02-03 14:22:46 +0100

    test: added languageSelector

---
+++ tests/components/LanguageSelector.spec.js
@@ -0,0 +1,35 @@
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+
+import LanguageSelector from '@/components/LanguageSelector.vue'
+
+Vue.use(Vuetify)
+
+function render () {
+  const div = document.createElement('div')
+  document.body.append(div)
+  return mount(LanguageSelector, {
+    stubs: ['Flag'],
+    vuetify: new Vuetify(),
+    attachTo: div,
+    mocks: {
+      $gettext: utils.mocks.translate,
+      $language: {
+        available: {
+          'en-US': { name: 'American English', code: 'US' },
+          'de-DE': { name: 'Deutsch', code: 'DE' }
+        },
+        current: 'de-DE'
+      }
+    }
+  })
+}
+
+describe('language selector', () => {
+  it('renders', () => {
+    const wrapper = render()
+    utils.debugDom(wrapper)
+    expect(wrapper.isVisible()).toBe(true)
+  })
+})
