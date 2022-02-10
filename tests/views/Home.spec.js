439148dcb9adc1bcd5a04a0322d9fbbe47b353a1
Anna <annagiulia.musso@gmail.com>
2022-02-10 10:57:48 +0100

    test: added render test to Home

--- tests/views/Home.spec.js
+++ tests/views/Home.spec.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 
-import Home from '@views/PostPage.vue'
+import Home from '@/views/Home.vue'
 import { testPost } from '../helpers/testHelpers'
 
 Vue.use(Vuetify)
@@ -14,10 +14,16 @@ function render () {
   const div = document.createElement('div')
   document.body.append(div)
   return mount(Home, {
+    stubs: ['Flag'],
+    vuetify: new Vuetify(),
     attachTo: div,
     mocks: {
       $gettext: utils.mocks.translate,
       $language: {
+        available: {
+          'en-US': { name: 'American English', code: 'US' },
+          'de-DE': { name: 'Deutsch', code: 'DE' }
+        },
         current: 'de-DE'
       },
       $store: new Vuex.Store({
@@ -37,5 +43,16 @@ describe('Home', () => {
     document.body.innerHTML = null
     jest.clearAllMocks()
   })
-  it('renders')
+  it('renders', () => {
+    const wrapper = render()
+
+    expect(wrapper.isVisible()).toEqual(true)
+  })
+
+  it.only('shows fallback text if there are no posts', () => {
+    const wrapper = render()
+    const fallback = wrapper.find('h1')
+
+    expect(fallback.text()).toBe('**There are no blog posts**')
+  })
 })
