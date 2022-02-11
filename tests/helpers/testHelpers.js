9834da7c07a3fdc98d023f039fc99c75e705f96a
Anna <annagiulia.musso@gmail.com>
2022-02-11 16:01:50 +0100

    test: added 3rd dummy post

--- tests/helpers/testHelpers.js
+++ tests/helpers/testHelpers.js
@@ -6,7 +6,7 @@ const testPost = {
   category: 'environment',
   title: 'hello',
   content: 'blabla',
-  creationDate: new Date(),
+  creationDate: new Date(2020, 0),
   id: 'abcde'
 }
 const testPost2 = {
@@ -17,8 +17,19 @@ const testPost2 = {
   category: 'politics',
   title: 'this is a title',
   content: 'this is the content',
-  creationDate: new Date(),
+  creationDate: new Date(2021, 3),
   id: 'xoxo'
 }
+const testPost3 = {
+  firstName: 'name',
+  middleName: 'middle',
+  lastName: 'surname',
+  email: 'email@email',
+  category: 'environment',
+  title: 'titletitle',
+  content: 'blablablabla bla',
+  creationDate: new Date(2019, 7),
+  id: 'ididid'
+}
 
-export { testPost, testPost2 }
+export { testPost, testPost2, testPost3 }
--- tests/views/Home.spec.js
+++ tests/views/Home.spec.js
@@ -4,7 +4,7 @@ import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 
 import Home from '@/views/Home.vue'
-import { testPost, testPost2 } from '../helpers/testHelpers'
+import { testPost, testPost2, testPost3 } from '../helpers/testHelpers'
 
 Vue.use(Vuetify)
 
@@ -26,18 +26,11 @@ function render () {
         },
         current: 'de-DE'
       },
-      //   $store: new Vuex.Store({
-      //     strict: true,
-      //     state () {
-      //       return {
-      //         posts: []
-      //       }
-      //     }
-      //   })
       $store: {
         state: {
-          posts: []
+          posts: [testPost, testPost2, testPost3]
         }
+        // i get an error if I don't define dispatch as a mock in the test
         // dispatch: jest.fn()
       }
     }
@@ -61,36 +54,93 @@ describe('Home', () => {
     expect(languageSelector.exists()).toBe(true)
   })
 
-  it('shows fallback text if there are no posts', () => {
+  it('shows fallback text if there are no posts', async () => {
     const wrapper = render()
+    wrapper.vm.$store.state.posts = []
+    await Vue.nextTick()
     const fallback = wrapper.find('h1')
 
     expect(fallback.text()).toBe('**There are no blog posts**')
   })
 
-  it('shows available posts', async () => {
+  it('shows available posts', () => {
     const wrapper = render()
-    wrapper.vm.$store.state.posts = [testPost, testPost2]
-    await Vue.nextTick()
-    // use findAll displayed posts
     const cards = wrapper.findAll('.v-card')
     // the number of displayed posts must equal the length of the posts array
     expect(cards.length).toEqual(wrapper.vm.$store.state.posts.length)
   })
 
-  it('displays the filters if there are posts', async () => {
+  it('displays the filters if there are posts', () => {
     const wrapper = render()
-    wrapper.vm.$store.state.posts = [testPost, testPost2]
-    await Vue.nextTick()
-    utils.debugDom(wrapper)
     const filtersContainer = wrapper.find('[data-cm-qa="filters-container"]')
     expect(filtersContainer.exists()).toBe(true)
   })
 
-  it.only('dispatches open input dialog to store', async () => {
+  it('dispatches open input dialog to store', async () => {
     const wrapper = render()
     const mockDispatch = wrapper.vm.$store.dispatch = jest.fn()
     await wrapper.find('button').trigger('click')
     expect(mockDispatch).toHaveBeenCalled()
+  })
+
+  it('calls updateFilters function correctly', async () => {
+    const spyUpdateFilters = jest.spyOn(Home.methods, 'updateFilters')
+    const spyFilterPosts = jest.spyOn(Home.methods, 'filterPosts')
+    const wrapper = render()
+
+    await wrapper.find('[data-cm-qa="category-select"]').trigger('input')
+    expect(spyUpdateFilters).toHaveBeenCalled()
+    expect(spyFilterPosts).toHaveBeenCalled()
+  })
+
+  it('filters posts by category', async () => {
+    const wrapper = render()
+    await wrapper.setData({
+      filters: {
+        category: testPost2.category,
+        date: false,
+        searchTerm: ''
+      }
+    })
+
+    const cards = wrapper.findAll('.v-card')
+    // search for the title in the first and only item in cards array
+    const postTitle = cards.at(0).find('.v-list-item__title')
+    expect(cards.length).toBe(1)
+    expect(postTitle.text()).toEqual(testPost2.title)
+  })
+
+  it('filters posts by search term', async () => {
+    const wrapper = render()
+    await wrapper.setData({
+      filters: {
+        category: '',
+        date: false,
+        searchTerm: testPost.content
+      }
+    })
+    const cards = wrapper.findAll('.v-card')
+    const post1Title = cards.at(0).find('.v-list-item__title')
+    const post2Title = cards.at(1).find('.v-list-item__title')
+    expect(cards.length).toBe(2)
+    expect(post1Title.text()).toEqual(testPost.title)
+    // testPost3 content includes search term
+    expect(post2Title.text()).toEqual(testPost3.title)
+  })
+
+  it('sorts posts by date', async () => {
+    const wrapper = render()
+    await wrapper.setData({
+      filters: {
+        category: '',
+        date: true,
+        searchTerm: ''
+      }
+    })
+
+    const cards = wrapper.findAll('.v-card')
+    // search for the title in the first and only item in cards array
+    const post1Title = cards.at(0).find('.v-list-item__title')
+    expect(post1Title.text()).toEqual(testPost2.title)
   })
 })
